00131ab0ab737af1eeb5428b9600c1a70bc8f58a
WawaDev16 <wawa.dev@gmail.com>
2024-05-29 20:18:18 +0800

    Set-up Hero Component

--- src/services/GlobalApi.tsx
+++ src/services/GlobalApi.tsx
@@ -1,10 +1,12 @@
-// https://api.themoviedb.org/3/discover/movie/550?api_key=2ec0d66f5bdf1dd12eefa0723f1479cf
+// https://api.themoviedb.org/3/discover/movie?api_key=2ec0d66f5bdf1dd12eefa0723f1479cf
 
 import axios from "axios";
 
-const movieBaseURL = "https://api.themoviedb.org/3/discover/movie?";
-const api_key = "2ec0d66f5bdf1dd12eefa0723f1479cf";
+const movieBaseURL = "https://api.themoviedb.org/3";
+const api_key = "896cbd9aff738d235d4631e016cb0edc";
 
-const getPopularMovies = axios.get(movieBaseURL + "/movie?api_key=" + api_key);
+const getPopularMovies = axios.get(
+  movieBaseURL + "/discover/movie?api_key=" + api_key
+);
 
 export default { getPopularMovies };
